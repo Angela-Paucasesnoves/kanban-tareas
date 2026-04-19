fc22940a3ed70cfc254eeb31910c9a8e864c0fff
Angela-Paucasesnoves <angelaramirez@paucasesnovescifp.cat>
2026-04-19 15:55:06 +0200

    feat: implementar renderizado del tablero Kanban en el DOM

--- js/script.js
+++ js/script.js
@@ -18,3 +18,35 @@ document.addEventListener("DOMContentLoaded", () => {
   cargarTareas();
   console.log("Tareas cargadas:", tareas);
 });
+
+function renderKanban() {
+  // limpiar columnas
+  document.getElementById("list-perFer").innerHTML = "";
+  document.getElementById("list-enCurs").innerHTML = "";
+  document.getElementById("list-fet").innerHTML = "";
+
+  // recorrer tareas
+  tareas.forEach((tarea) => {
+    const tarjeta = document.createElement("div");
+    tarjeta.classList.add("task-card");
+
+    tarjeta.innerHTML = `
+        <h4>${tarea.titulo}</h4>
+        <p>${tarea.descripcion || ""}</p>
+        <small>Prioridad: ${tarea.prioridad}</small>
+    `;
+
+    // colocar según estado
+    if (tarea.estado === "perFer") {
+      document.getElementById("list-perFer").appendChild(tarjeta);
+    }
+
+    if (tarea.estado === "perCurs") {
+      document.getElementById("list-perCurs").appendChild(tarjeta);
+    }
+
+    if (tarea.estado === "fet") {
+      document.getElementById("list-fet").appendChild(tarjeta);
+    }
+  });
+}
